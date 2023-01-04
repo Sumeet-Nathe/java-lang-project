d2e9394a9293f614aba6b287e2bc8d6ca614fbc8
Sumeet_Nathe <sumeet.m.nathe@gmail.com>
2023-01-04 19:42:59 +0530

    Added java8 and some logical impl.

--- src/co/in/an/eye/tech/java/concurrency/ExecutorsFramework.java
+++ src/co/in/an/eye/tech/java/concurrency/ExecutorsFramework.java
@@ -6,9 +6,7 @@ public class ExecutorsFramework {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-        executorService.execute(()->{
-            System.out.println("Using runnable");
-        });
+        executorService.execute(() -> System.out.println("Using runnable"));
         Future<?> submit1 = executorService.submit(() -> {
             String name = Thread.currentThread().getName();
             System.out.println("Current Thread : " + name);
